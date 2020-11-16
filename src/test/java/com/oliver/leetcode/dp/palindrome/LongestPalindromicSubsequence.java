4404a51369ac2045ae333026d5e170ab34716efb
enkeebbx <enkeebbx@gmail.com>
2020-11-16 10:05:32 +0900

    fix(516): tle 픽스

--- src/test/java/com/oliver/leetcode/dp/palindrome/LongestPalindromicSubsequence.java
+++ src/test/java/com/oliver/leetcode/dp/palindrome/LongestPalindromicSubsequence.java
@@ -2,32 +2,41 @@ package com.oliver.leetcode.dp.palindrome;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // 516
 public class LongestPalindromicSubsequence {
 
     @Test
     void check() {
-        String s = "cbbd";
+        String s = "aaa";
         int output = longestPalindromeSubseq(s);
         System.out.println(output);
     }
 
     public int longestPalindromeSubseq(String s) {
-        return longestPalindromeSubseqHelper(s, 0, s.length() - 1, 0);
+        return longestPalindromeSubseqHelper(s, 0, s.length() - 1, new HashMap<>());
     }
 
-    private int longestPalindromeSubseqHelper(String s, int i, int j, int size) {
+    private int longestPalindromeSubseqHelper(String s, int i, int j, Map<String, Integer> memo) {
+        if (i > j) {
+            return 0;
+        }
         if (i == j) {
             return 1;
         }
-        if (i > j) {
-            return size;
+
+        String key = i + "|" + j;
+
+        if (!memo.containsKey(key)) {
+            if (s.charAt(i) == s.charAt(j)) {
+                memo.put(key, longestPalindromeSubseqHelper(s, i + 1, j - 1, memo) + 2);
+            } else {
+                memo.put(key, Math.max(longestPalindromeSubseqHelper(s, i + 1, j, memo), longestPalindromeSubseqHelper(s, i, j - 1, memo)));
+            }
         }
-        if (s.charAt(i) == s.charAt(j)) {
-            return longestPalindromeSubseqHelper(s, i + 1, j - 1, size + 2);
-        } else {
-            return Math.max(longestPalindromeSubseqHelper(s, i + 1, j, size), longestPalindromeSubseqHelper(s, i, j - 1, size));
-        }
+        return memo.get(key);
     }
 
 }
